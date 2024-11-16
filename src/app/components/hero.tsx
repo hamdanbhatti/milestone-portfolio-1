3497f95b83814a9c6d19ca26b0d2226ac2a388fd
hamdanbhatti <hamdanbutt991@gmail.com>
2024-11-16 19:01:47 +0500

    Changes on photo

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -7,15 +7,15 @@ const Hero = () => {
    return (
       <section>
          <section className="text-gray-600 body-font">
-            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="container mx-auto flex px-5 py-1 md:flex-row flex-col items-center">
                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                   <h1 className="title-font sm:text-4xl text-3xl mb-8 font-medium text-gray-900">
-                     Hi
+                     Hi,
                   </h1>
                   <h1 className="title-font sm:text-4xl text-3xl mb-8 font-medium text-gray-900">
-                     I&apos;m
+                     I&apos;m 
                      <span className="text-indigo-600 font-bold hover:text-indigo-800 transition-colors duration-300">
-                        Hamdan
+                         Hamdan Bhatti
                      </span>
                      <br className="hidden lg:inline-block" />
                      <br className="lg:hidden" />
@@ -42,12 +42,12 @@ const Hero = () => {
                      </Link>
                   </div>
                </div>
-               <div className="lg:max-w-lg lg:w-full md:w-1/2 w-8/6">
+               <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                   <Image
                      className="object-cover object-center rounded-full"
                      alt="hero"
                      src="/images/hero-img.jpg"
-                     width={720}
+                     width={600}
                      height={600}
                   />
                </div>
